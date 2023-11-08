88b8d0ab8e2eb4ba684ae0452677a70bba4729b9
Elan Ruusamäe <glen@pld-linux.org>
2023-11-08 22:44:07 +0200

    fixup! Create etv.err.ee extractor

--- yt_dlp/extractor/etverr.py
+++ yt_dlp/extractor/etverr.py
@@ -7,9 +7,10 @@ from ..utils import (
 
 
 class ArhiivErrIE(InfoExtractor):
-    _VALID_URL = r'https://arhiiv\.err\.ee/video/vaata/(?P<id>.+)$'
+    # _VALID_URL = r'https://arhiiv\.err\.ee/video/vaata/(?P<id>.+)$'
+    _VALID_URL = r'https://etv\.err\.ee/(?P<id>\d+)/'
     _TESTS = [{
-        'url': 'https://arhiiv.err.ee/video/vaata/pealtnagija-481',
+        'url': 'https://etv.err.ee/1609138376/pealtnagija',
         'md5': '1ff59d535310ac9c5cf5f287d8f91b2d',
         'info_dict': {
             'id': 'pealtnagija-481',
@@ -26,16 +27,19 @@ class ArhiivErrIE(InfoExtractor):
 
     def _real_extract(self, url):
         video_id = self._match_id(url)
-        content_url = f"https://arhiiv.err.ee/api/v1/content/video/{video_id}"
+        # content_url = f"https://etv.err.ee/api/tv/getTvPageData?contentId={video_id}&parentContentId=1038446&categoryDataOnly=false"
+        content_url = f"https://etv.err.ee/api/tv/getTvPageData?contentId={video_id}"
         data = self._download_json(content_url, video_id)
 
         return {
             'id': video_id,
-            'title': traverse_obj(data, ('info', 'title')),
-            'description': traverse_obj(data, ('info', 'description')),
-            'formats': self._extract_m3u8_formats(traverse_obj(data, ('media', 'src', 'hls')), video_id),
-            'timestamp': unified_timestamp(traverse_obj(data, ('info', 'date'))),
-            'series': traverse_obj(data, ('info', 'seriesTitle')),
-            'episode': traverse_obj(data, ('info', 'title')),
-            'episode_number': int_or_none(traverse_obj(data, ('info', 'episode'))),
+            'title': traverse_obj(data, ('showInfo', 'programSubTitle')),
+            'description': traverse_obj(data, ('showInfo', 'programLead')),
+            'thumbnail': traverse_obj(data, ('showInfo', 'media', 'thumbnail', 'url')),
+            'formats': self._extract_m3u8_formats(traverse_obj(data, ('showInfo', 'media', 'src', 'hls')), video_id),
+            'timestamp': traverse_obj(data, ('seoData', 'ogPublishTime')),
+            'series': traverse_obj(data, ('showInfo', 'programName')),
+            'season_number': int_or_none(traverse_obj(data, ('pageControlData', 'mainContent', 'season'))),
+            'episode': traverse_obj(data, ('showInfo', 'programSubTitle')),
+            'episode_number': int_or_none(traverse_obj(data, ('pageControlData', 'mainContent', 'episode'))),
         }
