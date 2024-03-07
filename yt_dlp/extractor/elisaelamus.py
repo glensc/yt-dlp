64134e3f8369f2843a8044778406ab356fc7ebfe
Elan Ruusamäe <glen@pld-linux.org>
2024-03-07 18:06:52 +0200

    fixup! fixup! Add ElisaElamus info extractor

--- yt_dlp/extractor/elisaelamus.py
+++ yt_dlp/extractor/elisaelamus.py
@@ -3,12 +3,12 @@ import json
 from .common import InfoExtractor
 from .. import int_or_none
 from ..utils import (
-    traverse_obj,
+    traverse_obj, join_nonempty,
 )
 
 
 class ElisaElamusIE(InfoExtractor):
-    _VALID_URL = r'https://elisaelamus\.ee(?:/huub)?/seriaalid/(?P<id>crid:[^?]+)'
+    _VALID_URL = r'https://elisaelamus\.ee(?:/huub)?/seriaalid/(?P<id>crid[^?]+)\?episode=(?P<ep>crid[^?]+)'
     _TESTS = [{
         'url': 'https://elisaelamus.ee/seriaalid/crid:~~2F~~2Fschange~dotcom~~2F9469d800-ee7b-4e2a-8980-6303a3f68831?episode=crid%3A%7E%7E2F%7E%7E2Fschange.com%7E%7E2Fteeveekolm.ee%7E%7E2FTEEB2024020716150058',
         'md5': '1ff59d535310ac9c5cf5f287d8f91b2d',
@@ -30,17 +30,30 @@ class ElisaElamusIE(InfoExtractor):
         },
     }]
 
+    def episode_index(self, data, episode_id: str):
+        """
+        Return index of Series.ChildSeriesCollection,Series.Titles.Title.id that matches episode_id
+        """
+        id_only = episode_id.split(r'~~')[-1]
+        serie = traverse_obj(data, ('Series', 'ChildSeriesCollection', 'Series'))[0]
+        titles = traverse_obj(serie, ('Titles', 'Title'))
+        return [i for i, t in enumerate(titles, start=1) if t["id"].endswith(id_only)][0]
+
     def _real_extract(self, url):
-        video_id = self._match_id(url)
+        telecast_id, episode = self._match_valid_url(url).group('id', 'ep')
+        episode = episode.replace("%3A", ":").replace("%7E", "~")
+        video_id = join_nonempty(telecast_id, episode, delim='_')
         data = self.get_data(video_id)
 
         import json
         res = {
+            'id': video_id,
+            'age_limit':  18 if traverse_obj(data, ('Series', 'IsAdult')) else None,
             **traverse_obj(data, {
-                'title': 'heading',
-                'description': 'lead',
-                'release_year': ('year', {int_or_none}),
-                'timestamp': 'publicStart',
+                'title': ('Series', 'Name'),
+                'series':  ('Series', 'Name'),
+                'season':  ('Series', 'ChildSeriesCollection', 'Series', 'Name'),
+                'episode_number': self.episode_index(data, episode),
             })
         }
         print(json.dumps(res))
